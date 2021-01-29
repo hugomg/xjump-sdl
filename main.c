7e4de4761ae2e690db40f61e920e74949048954c
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2021-01-29 12:04:51 -0300

    Add GPLv3 license

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
-// TODO: GPL license
+// Copyright 1997-1999 Tatsuya Kudoh
+// Copyright 1997-1999 Masato Taruishi
+// Copyright 2015-2021 Hugo Gualandi
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <SDL.h>
 
